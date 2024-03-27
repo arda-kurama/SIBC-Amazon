01f8126d36ff1997873b31dd2b67a11d30f3f7fc
arda-kurama <ardakurama@gmail.com>
2024-03-27 18:20:53 -0400

    Added more button options, visual feedback, and mouse even listener for horizontal scrolling

---
+++ index.js
@@ -0,0 +1,33 @@
+const scrollable = document.querySelector('.content');
+
+const onDrag = (event) => {
+  // Ensure we only do this for pointers that don't have native
+  // drag-scrolling behavior and when the pointer is down.
+  if (event.pointerType == 'mouse') {
+    scrollable.scrollLeft = elementFrom - event.clientX + pointerFrom;
+  }
+};
+
+scrollable.addEventListener('pointerdown', (event) => {
+  // Ensure we only do this for pointers that don't have native
+  // drag-scrolling behavior.
+  if (event.pointerType == 'mouse') {
+    pointerDown = true;
+    // Set the position where the mouse is starting to drag from.
+    pointerFrom = event.clientX;
+    // Set the position of the element is scrolled from.
+    elementFrom = scrollable.scrollLeft;
+
+    // React on pointer move.
+    document.addEventListener('pointermove', onDrag);
+  }
+});
+
+// Stop reacting on pointer move when pointer is no longer clicked.
+document.addEventListener('pointerup', (event) => {
+  // Ensure we only do this for pointers that don't have native
+  // drag-scrolling behavior.
+  if (event.pointerType == 'mouse') {
+    document.removeEventListener('pointermove', onDrag);
+  }
+});
