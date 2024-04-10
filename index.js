2c61f57d3c43a5e48430a151434b99168ac35b0a
arda-kurama <ardakurama@gmail.com>
2024-04-10 18:22:21 -0400

    Added alexa overlay

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const scrollable = document.querySelector('.content');
+const scrollable = document.querySelector(".content");
 
 const onDrag = (event) => {
   // Ensure we only do this for pointers that don't have native
   // drag-scrolling behavior and when the pointer is down.
-  if (event.pointerType == 'mouse') {
+  if (event.pointerType == "mouse") {
     scrollable.scrollLeft = elementFrom - event.clientX + pointerFrom;
   }
 };
 
-scrollable.addEventListener('pointerdown', (event) => {
+scrollable.addEventListener("pointerdown", (event) => {
   // Ensure we only do this for pointers that don't have native
   // drag-scrolling behavior.
-  if (event.pointerType == 'mouse') {
+  if (event.pointerType == "mouse") {
     pointerDown = true;
     // Set the position where the mouse is starting to drag from.
     pointerFrom = event.clientX;
@@ -19,15 +19,23 @@ scrollable.addEventListener('pointerdown', (event) => {
     elementFrom = scrollable.scrollLeft;
 
     // React on pointer move.
-    document.addEventListener('pointermove', onDrag);
+    document.addEventListener("pointermove", onDrag);
   }
 });
 
 // Stop reacting on pointer move when pointer is no longer clicked.
-document.addEventListener('pointerup', (event) => {
+document.addEventListener("pointerup", (event) => {
   // Ensure we only do this for pointers that don't have native
   // drag-scrolling behavior.
-  if (event.pointerType == 'mouse') {
-    document.removeEventListener('pointermove', onDrag);
+  if (event.pointerType == "mouse") {
+    document.removeEventListener("pointermove", onDrag);
   }
 });
+
+function openAlexa() {
+  document.getElementById("alexa").style.display = "block";
+}
+
+function closeAlexa() {
+  document.getElementById("alexa").style.display = "none";
+}
